75c57fe11621c82b34548b9392e50eda0f12bef5
trianiyuli <trianiyuli79@gmail.com>
2024-05-14 14:23:08 +0700

    commit triani

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\KlienController;
+use App\Http\Controllers\AdminFormController;
+use App\Http\Controllers\EditBioController;
+use App\Http\Controllers\ProfilAdminController;
+use App\Http\Controllers\AuthController;
 
 
 
@@ -24,3 +28,7 @@ Route::get('registration', [AuthController::class, 'register']);
 Route::get('/billing', function () {
     return view('Admin.billing');
 });
+
+//admin profile triani
+Route::resource('profile', \App\Http\Controllers\ProfilAdminController::class);
+Route::get('/edit_profile', [ProfilAdminController::class, 'editProfile'])->name('edit_profile');
